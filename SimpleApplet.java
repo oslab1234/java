755dc5a75d62b6b81669c29c7bc39b9ed4d9617d
javalab1234 <88729310+javalab1234@users.noreply.github.com>
2021-08-10 20:50:41 +0530

    Create SimpleApplet.java

---
+++ SimpleApplet.java
@@ -0,0 +1,11 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="AppletDemo.class" width=300 height=400>
+</applet> */
+public class AppletDemo extends Applet
+{
+ public void paint(Graphics g)
+ { 
+ g.drawString("My Sample Applet program",70,200);
+ }
+}
